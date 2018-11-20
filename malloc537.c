b68cf40e08d22489a1b7b636b1ad63b9690630dc
Ian Mark <imark@royal-16.cs.wisc.edu>
2018-11-20 16:31:27 -0600

    Initialize

---
+++ malloc537.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void malloc537(size_t size) {
+
+}
+
+void free537(void *ptr) {
+
+}
+
+void memcheck537(void *ptr, size_t size) {
+
+}
+
+void realloc537(void *ptr, size_t size) {
+
+}
