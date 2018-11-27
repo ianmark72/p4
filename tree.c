39b3b1e40858a9afd7403fb69237700e24fb2747
Ian Mark <imark@royal-08.cs.wisc.edu>
2018-11-27 16:45:00 -0600

    hey there matt what's it like in matt city

--- tree.c
+++ tree.c
@@ -3,8 +3,27 @@
 
 typedef struct node {
     tuple* tuple;
-    int color;
+    int color; // 0 as black, 1 as red
     node* left;
     node* right;
     node* parent;
+    int status; //0 if malloc, 1 if freed
 } node;
+typedef struct rootNode {
+    node* root;
+} rootNode;
+
+rootNode* createTree(tuple* tuple) {
+    node firstNode;
+    firstNode = calloc(sizeof(node), 1);
+    firstNode.color = 0;
+    firstNode.left = NULL;
+    firstNode.right = NULL;
+    firstNode.parent = NULL;
+    firstNode.tuple = tuple;
+    firstNode.status = 0;
+
+    rootNode root = calloc(sizeof(rootNode), 1);
+    root.root = firstNode;
+    return root;
+}
