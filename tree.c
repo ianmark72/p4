3a8b78dc4a7704b9e35aca22ef7841379cadbbcb
Matthew Derzay <derzay@royal-09.cs.wisc.edu>
2018-11-27 16:47:08 -0600

    mexico is great this time of year

--- tree.c
+++ tree.c
@@ -8,3 +8,33 @@ typedef struct node {
     node* right;
     node* parent;
 } node;
+
+void addNode(node* node, tuple* tuple) {
+	if(node->tuple->address > tuple->address) {
+		//Left
+		if(node->left == NULL) {
+			node* newNode = calloc(1, sizeof(node));
+			newNode->color = 0;
+			newNode->parent = node;
+			newNode->left = NULL;
+			newNode->right = NULL;
+			newNode->tuple = tuple;
+			node->left = newNode;
+		}else{
+			addNode(node->left, tuple);
+		}
+	}else if(node->tuple->address <= tuple->address) {
+		//Right
+		if(node->right == NULL) {
+			node* newNode = calloc(1, sizeof(node));
+                        newNode->color = 0;
+                        newNode->parent = node;
+                        newNode->left = NULL;
+                        newNode->right = NULL;
+                        newNode->tuple = tuple;
+                        node->right = newNode;
+		}else{
+			addNode(node->right, tuple);
+		}
+	}
+}
