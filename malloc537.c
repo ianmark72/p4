78697c98677980e26accc0a82a69230d7779b2c2
Matthew Derzay <derzay@royal-30.cs.wisc.edu>
2018-11-27 10:59:47 -0600

    did the thing

--- malloc537.c
+++ malloc537.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void malloc537(size_t size) {
+typedef struct tuple {
+	int* address;
+	int length;
+} tuple;
 
+void malloc537(size_t size) {
+	tuple* memInfo = malloc(sizeof(tuple));
+
+	memInfo->address = malloc(size);
+	memInfo->length = size;
 }
 
 int main() {
