b441fffc3e7b75518538391dbc0e1469a27bfcdf
Matthew Derzay <derzay@royal-09.cs.wisc.edu>
2018-11-27 15:07:04 -0600

    Return to one file

--- malloc537.c
+++ malloc537.c
@@ -13,6 +13,18 @@ void malloc537(size_t size) {
 	memInfo->length = size;
 }
 
+void free537(void *ptr) {
+
+}
+
+void memcheck537(void *ptr, size_t size) {
+
+}
+
+void realloc537(void *ptr, size_t size) {
+
+}
+
 int main() {
 
 }
